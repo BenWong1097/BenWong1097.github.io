311283a4a762d9c8f0e04f95593c7e635f919252
benjaminwong <ben.wong.jamin@gmail.com>
2017-08-18 15:43:35 -0500

    Fleshing out Update

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,8 +16,8 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'benjamin@bwong.me'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "[bwong.me] Personal Website Contact Form:  $name";
+$to = 'ben.wong.jamin@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "bwong.me - Personal Website Contact Form:  $name";
 $email_body = "You have received a new message from your personal website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@bwong.me\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
