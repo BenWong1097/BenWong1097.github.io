0e839e3f4923ce43def8534f345346a069310214
railford <railford@yahoo.com>
2018-01-09 12:30:06 -0600

    Tweeked modal status updater

--- js/bwong.js
+++ js/bwong.js
@@ -1,12 +1,24 @@
 //A workaround to bootstrap's data-toggle limitations
-$("div.nav-arrow2").click(async function(){
-	//Wait for things to be set, before fixing it!
-	let wait = ms=> new Promise(r=>setTimeout(r, ms));
-	await wait(600);
+// var resetModal = async function(){
+// 	//Wait for things to be set, before fixing it!
+// 	let wait = ms=> new Promise(r=>setTimeout(r, ms));
+// 	await wait(1000);
+// 	//Setup correct properties to fix scrolling
+// 	var page_top = document.getElementById('page-top');
+// 	page_top.style += " padding-right: 16px;";
+// 	page_top.className += " modal-open";
+// 	var mainNav = document.getElementById('mainNav');
+// 	mainNav.style.paddingRight = "32px";
+// };
+
+//Not using promises due to incompatibility... this'll do
+var resetModal = function(){
 	//Setup correct properties to fix scrolling
 	var page_top = document.getElementById('page-top');
 	page_top.style += " padding-right: 16px;";
 	page_top.className += " modal-open";
 	var mainNav = document.getElementById('mainNav');
 	mainNav.style.paddingRight = "32px";
-});
+}
+
+$("div.nav-arrow2").click(function(){setTimeout(resetModal, 500)});
